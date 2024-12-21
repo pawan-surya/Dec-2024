3a0a6f4f2df396aa6a04f5c1a602f185eaad34bb
pawansuryaji <66516775+pawan-surya@users.noreply.github.com>
2024-12-21 19:37:33 +0530

    changes

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 // Your routes and middleware go here how to check
-app.get('/', (req, res) => res.status(200).send('OK'));
+app.get('/', (req, res) => res.status(200).send('My Node js First Add using CI-CD'));
 
 module.exports = app;  // Export the app instance for testing
