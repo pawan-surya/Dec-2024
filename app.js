c8ebed8c1d6a011aebd844d5d6235f1a1537eb4f
pawansuryaji <66516775+pawan-surya@users.noreply.github.com>
2024-12-21 19:30:33 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-// Your routes and middleware go here
+// Your routes and middleware go here how to check
 app.get('/', (req, res) => res.status(200).send('OK'));
 
 module.exports = app;  // Export the app instance for testing
