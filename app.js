a9e07c249a12071708a443d718af4d2478f23484
pawansuryaji <66516775+pawan-surya@users.noreply.github.com>
2024-12-21 15:37:09 +0530

    changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const app = require('express')
+const express = require('express');  // Import the express module
+const app = express();  // Create an instance of the express app
+
 app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on port 3000');
-  });
-  
+  console.log('Server is running on port 3000');
+});
